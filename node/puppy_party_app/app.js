5c1f060ba6790902d0ff35ca168627ea94b28b5a
christina regis <Mcfreegis11!>
2016-04-22 11:03:36 -0700

    added public folder for static assets

--- node/puppy_party_app/app.js
+++ node/puppy_party_app/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
+var homeRouter = require('./routes/home_router.js');
 var port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -10,10 +11,12 @@ app.use(bodyParser.json());
 
 app.use(morgan('combined'));
 
-app.get('/', function(req, res){
+//serve static assets- put above routes
+app.use(express.static('public'));
+
+router.get('/', function(req, res){
   res.json({message: "hello from index"});
 });
-
 
 app.listen(port, function(req, res){
   console.log("zzz snoring puppies");

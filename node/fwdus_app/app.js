4081fe1bf12c06fffd3cd35cfc621667bec60d6a
christina regis <Mcfreegis11!>
2016-04-25 11:21:56 -0700

    connect css and js for hackathon app

--- node/fwdus_app/app.js
+++ node/fwdus_app/app.js
@@ -2,36 +2,26 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
-var homeRouter = require('./routes/home_router.js');
 var port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended: true}));
-
 app.use(bodyParser.json());
 
 app.use(morgan('combined'));
 
 //serve static assets- put above routes
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 //use ejs for templates
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
-  var greetings = ["hello", "hi", "let's party"];
-  var greeting = greetings[Math.floor((Math.random()* greetings.length))];
   //index refers to index.ejs file, is a path when in another foler ex /public/index.ejs
   //greeting is a variable
-  res.render('index', {title: 'hey', message: 'hello there', greeting: greeting});
+  res.render('index', {message: 'hello there'});
 });
 
-// app.get('/', function(req, res){
-//   res.json({message: "hello from index"});
-// });
 
-app.get('/puppies', function(req, res){
-  var puppies = ["Winston", "Lucy", "Sammy", "Spot", "Cuddles", "Bently", "Ethel"];
-  res.render('puppies/index', {puppies: puppies});
-});
+
 
 app.listen(port, function(req, res){
   console.log("zzz snoring puppies");
